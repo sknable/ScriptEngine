c4a756d249f00ce6ee6c4c959c757d9d13771f40
Steve Knable <steve@steveknable.com>
2013-10-30 22:21:15 -0500

    Init Version

--- CScript/CScript/CScriptEngine.cs
+++ CScript/CScript/CScriptEngine.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mono.CSharp;
 
 namespace CScript
 {
     public class CScriptEngine
     {
+
+        private Evaluator sharpCompiler;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public CScriptEngine()
+        {
+            CompilerContext context = new CompilerContext(new CompilerSettings(),new ConsoleReportPrinter());
+            sharpCompiler = new Evaluator(context);
+        }
+
+        public object Eval(String line)
+        {
+
+            object result;
+            bool resultSet;
+
+            sharpCompiler.Evaluate(line,out result, out resultSet);
+
+            if (resultSet)
+            {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
     }
 }
---
+++ CScript/Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CScript;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CScriptEngine sharpEngine = new CScriptEngine();
+
+            Console.WriteLine(sharpEngine.Eval("int i = 0;"));
+            Console.WriteLine(sharpEngine.Eval("i++;"));
+            Console.WriteLine(sharpEngine.Eval("i++;"));
+            Console.WriteLine(sharpEngine.Eval("i++;"));
+            Console.WriteLine(sharpEngine.Eval("i++;"));
+
+            Console.ReadLine();
+        }
+    }
+}
